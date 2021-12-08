ec518d961edc32861e87194a5df9b00f7fbd98e3
kaviramsv <kavitasvenkitachalam@gmail.com>
2021-12-07 20:35:02 -0500

    Set usestate in app.js to set the day and and pass it to DayList.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,33 @@
-import React from "react";
-
+import React, { useState } from "react";
 import "components/Application.scss";
+import DayList from "./DayList";
 
-export default function Application(props) {
+
+
+
+  const days = [
+    {
+      id: 1,
+      name: "Monday",
+      spots: 2,
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      spots: 5,
+    },
+    {
+      id: 3,
+      name: "Wednesday",
+      spots: 0,
+    },
+  ];
+  
+  
+  export default function Application(props) {
+
+   const[day,setDay]=useState("Monday");
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -10,7 +35,10 @@ export default function Application(props) {
            src="images/logo.png"
            alt="Interview Scheduler"/>
       <hr  className="sidebar__separator sidebar--centered" />
-      <nav className="sidebar__menu"></nav>
+      <nav className="sidebar__menu">
+
+      <DayList days={days} day={day}setDay={setDay}/>
+      </nav>
       <img className="sidebar__lhl sidebar--centered"
            src="images/lhl.png"
            alt="Lighthouse Labs" />
