d4c546e69efad8a328f853a319d7bb8991b11354
kaviramsv <kavitasvenkitachalam@gmail.com>
2021-12-08 19:59:40 -0500

    Refractor Day list components

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,17 +1,15 @@
 import React from  "react"
 import DayListItem from "./DayListItem"
 
-
 export default function DayList(props) {
-  const daysMapped = props.days.map(day => {
+  const daysMapped = props.days.map(day => {    
     return(
         <DayListItem 
           key={day.id}
           name={day.name} 
           spots={day.spots} 
-          selected={day.name === props.day}
-          setDay={props.setDay} 
-         
+          selected={day.name === props.value}
+          setDay={props.onChange} 
         />
     )
   })
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -11,15 +11,15 @@ export default function DayListItem(props) {
   const formatSpots = (spots) =>{
     if(spots===0){
       return "no spots remaining";
-    }else if(spots===1){
-      return "1 spot remaining";
-    }else{
-      return `${spots} spots remaining`
     }
-
+    if(spots===1){
+      return "1 spot remaining";
+    }    
+    return `${spots} spots remaining`;    
   }
-  return (
-    <li className={selectClass} onClick={() => props.setDay(props.name)}>
+
+ return (
+    <li className={selectClass} onClick={() => props.setDay(props.name)}  selected={props.selected}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
