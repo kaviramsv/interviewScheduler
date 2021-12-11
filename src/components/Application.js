8226c2dadc7956578cff955cb91c1e74524ec784
kaviramsv <kavitasvenkitachalam@gmail.com>
2021-12-11 16:19:18 -0500

    import usevisualmode hook and show,empty create functionality

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,22 +3,19 @@ import "components/Application.scss";
 import Appointment from "components/Appointment";
 import DayList from "./DayList";
 import axios from "axios";
-import { getAppointmentsForDay} from "helpers/selectors";
-
-
-
+import { getAppointmentsForDay ,getInterview} from "helpers/selectors";
 
 
 export default function Application(props) {
 
-
-  const [state, setState] = useState({ day: "Monday",
-                                       days: [],    
-                                       appointments: {}
-                                    });
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
 
   // const dailyAppointments = [];     
-                             
+
   const setDay = day => setState({ ...state, day });
   // const setDays = days => setState(prev => ({ ...prev, days }));
   // useEffect(() => {
@@ -31,57 +28,31 @@ export default function Application(props) {
       axios.get('http://localhost:8001/api/interviewers')
     ]).then((all) => {
       // set your states here with the correct values...
-      console.log('O',all[0]); // first
-      console.log('1',all[1]); // second
-      console.log('2',all[2]); // third
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
-     
+      // console.log('O',all[0]); // first
+      // console.log('1',all[1]); // second
+      //console.log('2', all[2]); // third
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+
 
     })
   }, []);
 
 
- const dailyAppointments = getAppointmentsForDay(state, state.day);
- console.log(dailyAppointments);
-// [
-//   {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 3,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Andrews",
-//       interviewer: {
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       }
-//     }
-//   },
-//   {
-//     id: 5,
-//     time: "4pm",
-//   }
-// ];
- 
+  const appointments = getAppointmentsForDay(state, state.day);
+  const schedule = appointments.map((appointment) => {
+  const interview = getInterview(state, appointment.interview);
+  
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
+
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -98,11 +69,48 @@ export default function Application(props) {
 
       </section>
       <section className="schedule">
-        {dailyAppointments.map(appointment => {
-          return (<Appointment key={appointment.id} {...appointment} />)
-        })}
+
+        {schedule}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
 }
+// [
+  //   {
+  //     id: 1,
+  //     time: "12pm",
+  //   },
+  //   {
+  //     id: 2,
+  //     time: "1pm",
+  //     interview: {
+  //       student: "Lydia Miller-Jones",
+  //       interviewer: {
+  //         id: 3,
+  //         name: "Sylvia Palmer",
+  //         avatar: "https://i.imgur.com/LpaY82x.png",
+  //       }
+  //     }
+  //   },
+  //   {
+  //     id: 3,
+  //     time: "2pm",
+  //   },
+  //   {
+  //     id: 4,
+  //     time: "3pm",
+  //     interview: {
+  //       student: "Archie Andrews",
+  //       interviewer: {
+  //         id: 4,
+  //         name: "Cohana Roy",
+  //         avatar: "https://i.imgur.com/FK8V841.jpg",
+  //       }
+  //     }
+  //   },
+  //   {
+  //     id: 5,
+  //     time: "4pm",
+  //   }
+  // ];
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,17 +4,38 @@ import Show from "components/Appointment/Show.js";
 import Empty from "components/Appointment/Empty.js";
 
 import "../Appointment/styles.scss";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
 
 
 export default function Appointment(props) {
-
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <article className="appointment">
       {/* {props.time ? <p>Appointment at {props.time}</p> : <p>No appointments</p>} */}
 
       <Header time={props.time}/>
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/>:       
-      <Empty />}
+      {mode === EMPTY && 
+      <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+      <Show student={props.interview.student} interviewer={props.interview.interviewer}
+       /> )}
+       {mode === CREATE && (
+      <Form  
+      onCancel={back}
+      interviewers={[]}
+      interviewer={props.interviewer}
+     
+    
+       /> )}
+      {/* {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/>:       
+      <Empty />} */}
     </article>
   );
 
