8040fc596b626f06eb3d428e1219465299432a83
kaviramsv <kavitasvenkitachalam@gmail.com>
2021-12-10 21:39:28 -0500

    Add helper function to get interviewer details

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,20 +1,42 @@
 export function getAppointmentsForDay(state, day) {
-   
 
-   const day_array = state.days.filter(item=>item.name===day);
-   if(day_array.length===0){
+   const day_array = state.days.filter(item => item.name === day);
+   if (day_array.length === 0) {
       return [];
    }
-
-   const appt_arr = day_array.map(item=>  item.appointments);
-   const data =[];
- 
-  for(const appt of appt_arr[0]){   
-    
-   data.push(state.appointments[appt])
-  }
-   console.log(data);
+   const appt_arr = day_array.map(item => item.appointments);
+   const data = [];
+   for (const appt of appt_arr[0]) {
+      data.push(state.appointments[appt])
+   }   
    return data;
-  
 
 }
+
+export function getInterview(state,interview) {
+if(!interview){
+   return null;
+}
+
+const id=interview.interviewer;//incoming data
+const student = interview.student;//incoming data
+
+const details= state.interviewers[id];//need to fetch from interviewers
+
+const new_obj ={};//construct new obj
+new_obj['student']=student;
+new_obj['interviewer']=details;
+console.log(new_obj);
+return new_obj;
+}
+
+//  i/p====>{ student: "Archie Cohen", interviewer: 2 }
+
+//  o/p====>{
+//         student: 'Archie Cohen',
+//         interviewer: {
+//           id: 2,
+//           name: 'Tori Malcolm',
+//           avatar: 'https://i.imgur.com/Nmx0Qxo.png'
+//         }
+//       }
