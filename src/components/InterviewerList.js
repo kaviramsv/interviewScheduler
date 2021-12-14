a9a48c77440e0ab5a2060b9e5d4429b5a263b137
kaviramsv <kavitasvenkitachalam@gmail.com>
2021-12-14 17:31:26 -0500

    Validate array using prop types

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,26 +1,27 @@
 import React from "react";
-import classNames from "classnames";
+import PropTypes from 'prop-types';
 import "components/InterviewerList.scss";
 import InterviewerListItem from "components/InterviewerListItem.js";
 
 export default function InterviewerList(props) {
-  const interviewerMapped = props.interviewers.map(interviewer => {   
-  return (
-  <InterviewerListItem 
-      key ={interviewer.id}  
-      name={interviewer.name}
-      avatar={interviewer.avatar}        
-      selected={interviewer.id === props.value}
-      setInterviewer={()=>props.onchange(interviewer.id)}      
-      />   
-    )
-    }
-  )
+  const interviewerMapped = props.interviewers.map(interviewer => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.value}
+        setInterviewer={() => props.onchange(interviewer.id)}
+      />)
+  })
 
-return(
-<section className="interviewers">
-  <h4 className="interviewers__header text--light">Interviewer</h4>
-  <ul className="interviewers__list">{interviewerMapped}</ul>
-</section>
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewerMapped}</ul>
+    </section>
   );
 }
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
