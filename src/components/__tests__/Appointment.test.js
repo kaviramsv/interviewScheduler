742f1020f4620267b4d7ccdea14c2ad0c9e0436c
kaviramsv <kavitasvenkitachalam@gmail.com>
2021-12-14 22:47:51 -0500

    Add test cases for appointment component

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -4,8 +4,11 @@ import Appointment from "components/Appointment";
 
 
 afterEach(cleanup);
+
 describe("Appointment", () => {
+
   it("renders without crashing", () => {
+    
     render(<Appointment />);
   });
 });
